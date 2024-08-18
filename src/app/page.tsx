4a07f36e8974c79ccf4d6ea826529a0f6fa379c6
jinyangTF <jinyang@tinyfish.io>
2024-08-17 20:54:42 -0700

    Update

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import HeaderButtons from "@/components/shared/header/header-buttons";
+
+export default function Home() {
+  return (
+    <div className={`flex flex-col h-screen w-full p-[2%]`}>
+      <HeaderButtons/>
+    </div>
+  );
+}
