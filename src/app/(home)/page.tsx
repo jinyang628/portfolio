1a51d1156bdcaca29f9b40eab84175a816f8d9f8
jinyangTF <jinyang@tinyfish.io>
2024-11-30 11:22:26 -0800

    Update

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,27 +2,17 @@
 
 import { useState } from 'react';
 
-import LanguagesSection from '@/components/languages/languages-section';
 import MenuSection from '@/components/menu/menu-section';
-import TechStackSection from '@/components/tech-stack/tech-stack';
 import Title from '@/components/title';
 
 export default function Home() {
   const [showIntro, setShowIntro] = useState<boolean>(false);
   const [showCards, setShowCards] = useState<boolean>(false);
-  const [showTechStack, setShowTechStack] = useState<boolean>(false);
-  const [showLanguages, setShowLanguages] = useState<boolean>(false);
 
   const updateShowContent = () => {
     setShowIntro(true);
     setTimeout(() => {
       setShowCards(true);
-      setTimeout(() => {
-        setShowTechStack(true);
-        setTimeout(() => {
-          setShowLanguages(true);
-        }, 1000);
-      }, 1000);
     }, 1000);
   };
 
@@ -30,8 +20,6 @@ export default function Home() {
     <>
       <Title showIntro={showIntro} updateShowContent={updateShowContent} />
       <MenuSection showCards={showCards} />
-      <TechStackSection showTechStack={showTechStack} />
-      <LanguagesSection showLanguages={showLanguages} />
     </>
   );
 }
--- src/components/languages/languages-section.tsx
+++
@@ -1,7 +0,0 @@
-type LanguagesSectionProps = {
-  showLanguages: boolean;
-};
-
-export default function LanguagesSection({ showLanguages }: LanguagesSectionProps) {
-  return <p>My languages</p>;
-}
