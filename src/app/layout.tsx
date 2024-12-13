d16944ada564d48ab1024481bdc8419d852a94b1
jinyang628 <chenjinyang4192@gmail.com>
2024-12-12 16:52:19 -0800

    Update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,6 +32,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7810723596683853"
+          crossOrigin="anonymous"
+        ></script>
+      </head>
       <body className={inter.className}>
         <script
           type="module"
